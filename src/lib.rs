b19a8db9987fa1b3fc1e7e7703a91265e3ac34f7
Александр Сергиенко <s_sandr06@mail.ru>
2023-02-20 22:15:35 +0300

    Реализация перемещения некоторых типов файлов (типы вшиты в программу)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-use std::path::PathBuf;
-use std::fs::metadata;
-use ansi_term::Color::Red;
+use std::path::{Path, PathBuf};
+use std::fs::{DirEntry, metadata, ReadDir};
+use ansi_term::Color::*;
+use std::{env, fs, process};
 
 pub fn path_is_file(path: &PathBuf) -> bool {
     metadata(&path.display().to_string()).unwrap().is_file()
@@ -8,4 +9,41 @@ pub fn path_is_file(path: &PathBuf) -> bool {
 
 pub fn display_error(message: &str) {
     println!("{}: {}", Red.paint("Ошибка"), message);
+}
+
+pub fn display_warning(message: &str) {
+    println!("{}: {}", Yellow.paint("Предупреждение"), message);
+}
+
+pub fn enumerate_files() -> ReadDir {
+    let files = match fs::read_dir(env::current_dir().unwrap()) {
+        Ok(value) => { value }
+        Err(_) => {
+            display_error(&format!("не удалось перечислить файлы в текущей папке"));
+            process::exit(1);
+        }
+    };
+    files
+}
+
+pub fn move_file(path: &&DirEntry, new_directory: &str) {
+    create_directory(&new_directory);
+
+    let destination = get_destination(path, new_directory);
+    if let Err(e) = fs::rename(&path.file_name(), &destination) {
+        display_error(&format!("не удалось переместить файл \"{}\" в папку \"{}\" ({})", path.file_name().to_string_lossy(), new_directory, e.to_string()))
+    }
+}
+
+fn create_directory(directory: &&str) {
+    let path_to_new_folder = format!("{}/{}", env::current_dir().unwrap().to_str().unwrap(), directory);
+    if !Path::new(&path_to_new_folder).exists() {
+        if let Err(e) = fs::create_dir(&path_to_new_folder) {
+            display_error(&format!("не удалось создать папку \"{}\" ({})", directory, e.to_string()));
+        }
+    }
+}
+
+fn get_destination(path_to_file: &DirEntry, new_folder: &str) -> String {
+    format!("{}/{}/{}", env::current_dir().unwrap().display(), new_folder, path_to_file.file_name().to_string_lossy())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,69 @@
-use std::{env, fs, process};
-use std::fs::ReadDir;
-
-use cleanup::{display_error, path_is_file};
+use std::fs::DirEntry;
+use cleanup::{display_warning, path_is_file};
 
 
 mod args;
+mod moving_rules;
 
 fn main() {
-    let files = enumerate_files();
+    let files = cleanup::enumerate_files();
+
 
     for file in files {
-        let path = file.unwrap().path();
+        let path = &file.unwrap();
 
-        if path_is_file(&path) {
-            match &path.extension() {
+        if path_is_file(&path.path()) {
+            match &path.path().extension() {
                 None => {
-                    display_error(&format!("файл \"{}\" не имеет расширения.",
-                                            path.file_name()
-                                                .unwrap()
-                                                .to_string_lossy()
-                    ))
+                    moving_rules::move_misc(&path);
                 }
 
                 Some(extension) => {
-                    println!("{}", extension.to_string_lossy());
+                    match extension.to_str() {
+                        Some(value) => {
+                            handle_file_extension(&path, value)
+                        }
+                        None => {
+                            handle_missing_extension(&path)
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-fn enumerate_files() -> ReadDir {
-    let files = match fs::read_dir(env::current_dir().unwrap()) {
-        Ok(value) => { value }
-        Err(_) => {
-            display_error(&format!("не удалось перечислить файлы в текущей папке"));
-            process::exit(1);
+fn handle_file_extension(path: &&DirEntry, value: &str) {
+    match value {
+        "txt" => moving_rules::move_misc(&path),
+
+        "docx" | "xlsx" | "pptx"
+        | "doc" | "xls" | "ppt"
+        | "odt"
+        | "pdf" => moving_rules::move_document(&path),
+
+        "iso" => moving_rules::move_disk_image(&path),
+
+        "mp3" | "wav" | "flac" | "ogg" => moving_rules::move_music(&path),
+
+        "c" | "cpp" | "cs" | "rs" | "py" | "js" | "html" | "css" => moving_rules::move_source_code(&path),
+
+        "sh" | "sh1" => moving_rules::move_shell_script(&path),
+
+        "mp4" | "mov" => moving_rules::move_video(&path),
+
+        "zip" | "xz" | "gz" | "7z" => moving_rules::move_archive(&path),
+
+        "jpg" | "jpeg" | "png" | "heic" | "tga" | "dds" => moving_rules::move_image(&path),
+
+        _ => {
+            display_warning(&format!("файл {} имеет неизвестный тип и будет перемещен в папку \"Прочее\".", &path.file_name().to_str().unwrap()));
+            moving_rules::move_misc(&path);
         }
-    };
-    files
+    }
+}
+
+fn handle_missing_extension(path: &&DirEntry) {
+    display_warning(&format!("не удается преобразовать расширение файла {} в UTF-8. Он будет перемещен в папку \"Прочее\".", &path.file_name().to_str().unwrap()));
+    moving_rules::move_misc(&path);
 }
