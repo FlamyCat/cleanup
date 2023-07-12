0ab3a5143b83204b387c2e5d7544620d2d7479af
FlamyCat <s_sandr06@mail.ru>
2023-07-12 16:09:56 +0300

    Предварительная версия, программа опирается на содержимое файла при определении его типа.

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,98 @@
-use std::fs::DirEntry;
-use cleanup::{display_warning, path_is_file};
+use std::{env, fs};
+use std::process::ExitCode;
 
+use file_format::{FileFormat, Kind};
+
+use args::Args;
+use args::Parser;
+use cleanup::{display_error, display_warning, move_file};
 
 mod args;
-mod moving_rules;
-mod json;
 
-fn main() {
-    let files = cleanup::enumerate_files();
+fn main() -> ExitCode {
+    let args = Args::parse();
 
+    let files = if let Some(dir) = args.dir {
+        dir
+    } else if let Ok(files) = env::current_dir() {
+        files.to_str().unwrap().to_string()
+    } else {
+        display_error("выбранная директория недоступна или не существует");
+        return ExitCode::from(1);
+    };
 
-    for file in files {
-        let path = &file.unwrap();
+    for file in fs::read_dir(files).unwrap() {
+        let file = file.unwrap();
 
-        if path_is_file(&path.path()) {
-            match &path.path().extension() {
-                None => {
-                    moving_rules::move_misc(&path);
-                }
+        if file.metadata().unwrap().is_dir() {
+            continue;
+        }
 
-                Some(extension) => {
-                    match extension.to_str() {
-                        Some(value) => {
-                            handle_file_extension(&path, value)
-                        }
-                        None => {
-                            handle_missing_extension(&path)
-                        }
-                    }
-                }
+        let file_format = FileFormat::from_file(file.file_name());
+
+        if let Err(_) = file_format {
+            display_warning(format!("ошибка при получении формата файла \"{}\", файл будет пропущен.", file.file_name().to_string_lossy()).as_str());
+            continue;
+        }
+
+        let file_format = file_format.unwrap();
+
+        let destination = match file_format.kind() {
+            Kind::Application | Kind::Geospatial | Kind::Rom => {
+                "Другое"
             }
-        }
+            Kind::Archive => {
+                "Архивы"
+            }
+            Kind::Audio => {
+                "Аудиозаписи"
+            }
+            Kind::Book => {
+                "Книги"
+            }
+            Kind::Certificate => {
+                "Сертификаты"
+            }
+            Kind::Compression => {
+                "Сжатые файлы"
+            }
+            Kind::Disk => {
+                "Образы дисков"
+            }
+            Kind::Document => {
+                "Документы"
+            }
+            Kind::Executable => {
+                "Исполняемые файлы"
+            }
+            Kind::Font => {
+                "Шрифты"
+            }
+            Kind::Image => {
+                "Изображения"
+            }
+            Kind::Model => {
+                "3D-модели и чертежи"
+            }
+            Kind::Package => {
+                "Пакеты"
+            }
+            Kind::Playlist => {
+                "Плейлисты"
+            }
+            Kind::Subtitle => {
+                "Субтитры"
+            }
+            Kind::Text => {
+                "Текстовые файлы"
+            }
+            Kind::Video => {
+                "Видео"
+            }
+        };
+
+        move_file(&file, destination);
     }
-}
 
-fn handle_file_extension(path: &&DirEntry, value: &str) {
-    match value {
-        "txt" => moving_rules::move_misc(&path),
-
-        "docx" | "xlsx" | "pptx"
-        | "doc" | "xls" | "ppt"
-        | "odt"
-        | "pdf" => moving_rules::move_document(&path),
-
-        "iso" => moving_rules::move_disk_image(&path),
-
-        "mp3" | "wav" | "flac" | "ogg" => moving_rules::move_music(&path),
-
-        "c" | "cpp" | "cs" | "rs" | "py" | "js" | "html" | "css" => moving_rules::move_source_code(&path),
-
-        "sh" | "sh1" => moving_rules::move_shell_script(&path),
-
-        "mp4" | "mov" => moving_rules::move_video(&path),
-
-        "zip" | "xz" | "gz" | "7z" => moving_rules::move_archive(&path),
-
-        "jpg" | "jpeg" | "png" | "heic" | "tga" | "dds" => moving_rules::move_image(&path),
-
-        _ => {
-            display_warning(&format!("файл \"{}\" имеет неизвестный тип и будет перемещен в папку \"Прочее\".", &path.file_name().to_str().unwrap()));
-            moving_rules::move_misc(&path);
-        }
-    }
-}
-
-fn handle_missing_extension(path: &&DirEntry) {
-    display_warning(&format!("не удается преобразовать расширение файла {} в UTF-8. Он будет перемещен в папку \"Прочее\".", &path.file_name().to_str().unwrap()));
-    moving_rules::move_misc(&path);
+    ExitCode::SUCCESS
 }
